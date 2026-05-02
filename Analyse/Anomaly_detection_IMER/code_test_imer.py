ab7704cade9989e053e322d2580b56f64e7b41e3
Ruth Christia Emmanuella Malamba Mboumba <ruth.malamba-mboumba@insa-lyon.fr>
2026-05-02 12:41:06 +0200

    Ajustement de la fonction
    Refactor compute_imer function to include persistence and clean signal processing. Update main execution to handle new parameters and adjust detection logic.

--- Analyse/Anomaly_detection_IMER/code_test_imer.py
+++ Analyse/Anomaly_detection_IMER/code_test_imer.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import os
-# Pour trouver un fichier qui n'est pas sous le dossier actuel
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import glob
 from Lecture_data.lecture_mseed import lecture_mseed
-import numpy as np
+from Analyse.Smoothing.eppf import eppf
+from Analyse.Smoothing.eps import eps
 
-def compute_imer(signal, fs, n1_ms, n2_ms, n3_ms, n_avg, snr_bas):
+import numpy as np
+###ESSAYER D'AFFICHER PLUSIEURS EVENEMENTS DIFFERENTS
+###trouver un evenment plus ponctuel
+### ajouter temps d'execution et cout énérgetique 
+
+def compute_imer(signal, fs, n1_ms=10, n2_ms=30, n3_ms=30, n_avg=10, persistance_ms=5.0, snr_bas=False):
     """
     Code IMER
     """
@@ -17,7 +20,8 @@ def compute_imer(signal, fs, n1_ms, n2_ms, n3_ms, n_avg, snr_bas):
     n3 = max(1, int(n3_ms * fs / 1000))
     
     N = len(signal)
-    energy = signal**2
+    signal_clean = signal - np.mean(signal)  # Centrage du signal
+    energy = signal_clean**2
 
     imer_raw = np.zeros(len(signal))
     mer12 = np.zeros(len(signal))
@@ -29,7 +33,7 @@ def compute_imer(signal, fs, n1_ms, n2_ms, n3_ms, n_avg, snr_bas):
         e1 = np.mean(energy[k : k + n1])
         e2 = np.mean(energy[k - n2 : k])
         er12=e1/(e2 + 1e-10 )
-        mer12[k]=er12 *(abs(signal[k]))**3
+        mer12[k]=er12 *(abs(signal_clean[k]))**3
 
 
     # ETAPE 2 / CALCUL DE LA MER13
@@ -37,7 +41,7 @@ def compute_imer(signal, fs, n1_ms, n2_ms, n3_ms, n_avg, snr_bas):
         e1 = np.mean(energy[i+n2 : i + n1+n2]) 
         e3 = np.mean(energy[i-n3 : i])
         er13=e1/(e3 + 1e-10)
-        mer13[i]=er13 * abs(signal[i])**3
+        mer13[i]=er13 * abs(signal_clean[i])**3
 
     # ETAPE 3 / Lissage de la MER12 et la MER 13
     kernel = np.ones(n_avg) / n_avg
@@ -54,26 +58,44 @@ def compute_imer(signal, fs, n1_ms, n2_ms, n3_ms, n_avg, snr_bas):
     # ETAPE 5 /  Seuillage et pointé
     debut = n2 + n3
     zone_active = imer_curve[debut:]
-    indices_detection = []
+    cooldown= n2 + n3  # Période de non-détection après un pointé
+    n_persist = max(1, int(persistance_ms * fs / 1000))  # Nombre d'échantillons consécutifs au-dessus du seuil pour valider un pointé
+
+
 
     if len(zone_active) > 0:
         imer_mean= np.mean(zone_active)
         if snr_bas:
             k = 3 # Seuil plus bas pour les signaux à faible SNR
         else:
-            k = 1 # Seuil standard
+            k = 1 # Seuil plus bas pour les signaux à faible SNR
 
         threshold = imer_mean * k
 
-        indices = np.where(zone_active > threshold)[0]
-        for idx in indices:
-            if len(indices_detection) == 0 or int(idx + debut) > indices_detection[-1] + fs * 10: # on attend 10s pour qu'il ne s'agisse pas du même évènement (ou alors s'il y a pas déjà d'autre détection)
-                indices_detection.append(int(idx + debut)) 
-        
+        above = zone_active > threshold # Tableau booléen indiquant les points au-dessus du seuil
+        i=0
+        picks=[]
+        while i < len(above) -n_persist +1 :
+            if np.all(above[i:i+n_persist] ):
+                pick_abs=int(i + debut)
+                picks.append(pick_abs)
+                i +=cooldown
+            else:  
+                i += 1
+
+        # indices = np.where(zone_active > threshold)[0]
+        # if len(indices) > 0:
+        #     for idx in indices:
+        #         picks.append(idx+debut)
+        # else:
+        #     None
+        # pick_idx = int(indices[0] + debut) if len(indices) > 0 else None
+    
     else:
         threshold = 0
+        pick = None
     
-    return imer_curve, threshold, indices_detection, ma12, ma13_shift
+    return imer_curve, threshold, picks, ma12, ma13_shift
 
 
 # ---UTILISATION ---
@@ -93,33 +115,52 @@ if __name__ == "__main__":
     #     np.sin(2 * np.pi * 50 * t[idx_arr:])
     #     * np.exp(-8 * (t[idx_arr:] - arrival_time))
     # )
-    # trace = noise + wave
+    # trace_brute = noise + wave
+    # trace = eps(trace_brute, window_size=5)
     # -------------------------------------------------------------------------
  
     # ---- Données réelles (décommenter si lecture mseed disponible) ----------
-    trace = lecture_mseed("event_CREF.mseed")[0]['data_samples']
-    fs    = lecture_mseed("event_CREF.mseed")[0]['sample_rate_hz']
-    t     = np.arange(len(trace)) / fs
-    # -------------------------------------------------------------------------
- 
-    # Paramètres fenêtres (à adapter selon la durée attendue du signal)
-    # Article field data : nt1=3.75 ms, nt2=nt3=15 ms
-    # Article synthetic  : nt1=200 éch., nt2=nt3=600 éch. à 10 kHz
-    nt1_ms  = 10    # ms — fenêtre courte
-    nt2_ms  = 30    # ms — fenêtres longues (nt2 = nt3)
-    nt3_ms  = 30    # ms
+    trace_brute = lecture_mseed("GUI_20251128_110349.mseed")[0]['data_samples']
+    fs    = lecture_mseed("GUI_20251128_110349.mseed")[0]['sample_rate_hz']
+    t     = np.arange(len(trace_brute)) / fs
+    trace = eps(trace_brute, window_size=5)
+    
+    # # -------------------------------------------------------------------------
+    
+    #---- Trace fourni par le tuteur pour des test-----------------
+    # data_folder = "trace_capteur"
+    # fs = 100  # sampling rate (Hz)
+
+    # # Concatenate data from all file in the data folder
+    # all_data = []
+    # for fpath in sorted(glob.glob(data_folder + "/geophone_*.dat")):
+    #     data = np.fromfile(fpath, dtype=np.int16)
+    #     all_data.append(data)
+    # trace_brute = np.array(np.concatenate(all_data), dtype=np.float64)
+    # trace = eps(trace_brute, window_size=5)
+    # t = np.arange(len(trace)) / fs
+    #--------------------------------------------------------------
+
+    # Paramètres fenêtres
+    D_ms = 1000             # durée estimée d'une impulsion en ms — seul paramètre à fournir
+
+
+    nt1_ms  = D_ms   # ms — fenêtre courte
+    nt2_ms  = 3 * D_ms    # ms — fenêtres longues (nt2 = nt3)
+    nt3_ms  = 3 * D_ms    # ms
     n_smth  = 10    # points pour le moving average
+    persistance_ms = max(1000/fs, nt1_ms/20)     # ms de persistance pour valider un pointé
 
 
  
-    curve, thresh, pick_idx, ma12, ma13_sh = compute_imer(
+    curve, thresh, picks, ma12, ma13_sh = compute_imer(
         trace, fs,
         n1_ms=nt1_ms, n2_ms=nt2_ms, n3_ms=nt3_ms,
-        n_avg=n_smth, snr_bas=False
+        n_avg=n_smth, persistance_ms=persistance_ms, snr_bas=False
     )
 
-    if pick_idx is not None:
-        print(f"Pointé IMER à : {t[pick_idx]:.4f} s  (échantillon {pick_idx})")
+    if picks:
+        print(f"{len(picks)} pointé(s) détecté(s) aux indices (échantillons): {picks}") 
     else:
         print("Aucun pointé détecté.")
 
@@ -130,9 +171,11 @@ if __name__ == "__main__":
  
     # Signal brut
     ax0 = axes[0]
-    ax0.plot(t, trace, color='k', lw=0.7, label="Signal brut")
-    if pick_idx is not None:
-        ax0.axvline(t[pick_idx], color='r', lw=1.5, ls='--', label=f"Pointé IMER ({t[pick_idx]:.3f} s)")
+    ax0.plot(t, trace_brute, color='grey', lw=0.7, label="Signal brut")
+    ax0.plot(t, trace, color='k', lw=0.7, label="Signal filtré")
+    if picks:
+        for p in picks:
+            ax0.axvline(t[p], color='r', lw=1.5, ls='--')
     ax0.set_ylabel("Amplitude")
     ax0.legend(fontsize=8)
     ax0.set_title("Signal sismique")
@@ -141,8 +184,6 @@ if __name__ == "__main__":
     ax1 = axes[1]
     ax1.plot(t, ma12,   color='steelblue', lw=1,   label="MA₁,₂")
     ax1.plot(t, ma13_sh, color='darkorange', lw=1, ls='--', label="MA₁,₃ (décalée nt2)")
-    if pick_idx is not None:
-        ax1.axvline(t[pick_idx], color='r', lw=1.5, ls='--')
     ax1.set_ylabel("MER lissé")
     ax1.legend(fontsize=8)
     ax1.set_title("Distributions lissées MA₁,₂ et MA₁,₃ (étape 3 & 4)")
@@ -151,8 +192,6 @@ if __name__ == "__main__":
     ax2 = axes[2]
     ax2.plot(t, curve, color='forestgreen', lw=1, label="IMER")
     ax2.axhline(thresh, color='orange', ls=':', lw=1.5, label=f"Seuil = mean(IMER) = {thresh:.3e}")
-    if pick_idx is not None:
-        ax2.axvline(t[pick_idx], color='r', lw=1.5, ls='--', label="Pointé")
     ax2.set_ylabel("IMER")
     ax2.set_xlabel("Temps (s)")
     ax2.legend(fontsize=8)
