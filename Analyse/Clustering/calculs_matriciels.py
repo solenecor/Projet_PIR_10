6751cb9cc6d2ebdbd720c51877fa299d8c23ca35
BrumeMer51 <laoipub51+github@gmail.com>
2026-05-05 17:24:27 +0200

    Ajout de la méthode k-nn pour garder un nombre limité de voisins

--- Analyse/Clustering/calculs_matriciels.py
+++ Analyse/Clustering/calculs_matriciels.py
@@ -102,6 +102,28 @@ def matrice_distance_globale_autres(series, f_distance) :
 
     return m_d
 
+### Fonction pour une méthode de grpahe k-NN (chaque sommet garde ses k voisins les plus proches)
+def knn_graph(m_s, k):
+    """
+    Tranforme la matrice de similarite m_s avec la méthode k-nn
+    Entrées : 
+        m_s : matrice numpy, array de array de float
+        k : entier
+    Sortie :
+        m_knn : matrice numpy, array de array de float
+    """
+    n = m_s.shape[0]
+    m_knn = np.zeros_like(m_s)
+
+    for i in range(n):
+        # On trie la ligne de i par ordre décroissant, et on garde que les k premiers voisins
+        idx = np.argsort(m_s[i])[::-1][1:k+1]
+        m_knn[i, idx] = m_s[i, idx]
+
+    # symétrisation (important pour graphe non orienté)
+    m_knn = np.maximum(m_knn, m_knn.T)
+
+    return m_knn
 
 ### Calcul de la matrice de similarité :
 def matrice_similarite(m_distance) :
--- Analyse/Clustering/fct_clustering_complet.py
+++ Analyse/Clustering/fct_clustering_complet.py
@@ -13,126 +13,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 from Lecture_data.lecture_mseed import *
 
 
-
-def clustering_visibility_graph(l_series, nb_seg = 30) :
-    """
-    Effectue le clustering sur les séries temporelles en utilisant les graphes de visibilité pondérés
-    Entrées :
-        l_series : array d'array numpy, chaque array correspond à une série temporelle
-        nb_seg : entier, nombre de segments à découper pour chaque série
-    Sorties :
-        G : graphe networkx
-        m_similarite : matrice numpy (array de array de float)
-    """
-    # Découpage en un même nombre de segments :
-    segments_tot = []
-    for serie in l_series :
-        segments_tot.append(decoupe_segments(serie, nb_seg))
-
-    l_vecteurs = [[] for _ in range(nb_seg)]
-
-    # Transfo de chaque segment en weighted visibility graph :
-    for serie in segments_tot :
-        for idx_seg in range(nb_seg) :
-            seg = serie[idx_seg]
-            vg = NaturalVG(weighted="abs_slope")  # poids = pente absolue entre les points
-            vg.build(seg)
-            edges = vg.edges
-            n = len(seg)
-            # Matrice d'adjacence du graphe :
-            adj_matrix = np.zeros((n, n))
-            for u,v,w in edges:
-                adj_matrix[u, v] = w
-                adj_matrix[v, u] = w  # symétrique
-            # Calcul du vecteur :
-            vect = []
-            for u in range (len(adj_matrix)) :
-                composante = 0
-                for v in range(len(adj_matrix)) :
-                    if adj_matrix[u, v] != 0 :
-                        composante += 1
-                vect.append(composante)
-            l_vecteurs[idx_seg].append(vect)
-
-
-    # Calcul des matrices pour chaque segment :
-    matrices = []
-    for i in range(len(l_vecteurs)) :
-        matrices.append(matrice_segment(l_vecteurs[i]))
-
-
-    # Calcul de la matrice de distance globale :
-    m_distance_globale = matrice_distance_globale(matrices)
-
-    
-    # Calcul de la matrice de similarité : 
-    m_similarite = matrice_similarite(m_distance_globale)
-
-    # Transfo en graphe : 
-    G = transfo_graphe(m_similarite)
-
-    return G, m_similarite 
-
-def clustering_distance_dtw(l_series) :
-    """
-    Effectue le clustering sur les séries temporelles en utilisant la distance DTW
-    Entrées :
-        l_series : liste d'array numpy, chaque array correspond à une série temporelle
-    Sorties :
-        G : graphe networkx
-        m_similarite : matrice numpy (array de array de float)
-    """
-    # Calcul de la matrice de distance globale :
-    m_distance_globale = matrice_distance_globale_autres(l_series, distance_dtw)
-
-    # Calcul de la matrice de similarité : 
-    m_similarite = matrice_similarite(m_distance_globale)
-
-    # # Transfo en graphe : 
-    G = transfo_graphe(m_similarite)
-
-    return G, m_similarite 
-
-def clustering_distance_L1(l_series, ) :
-    """
-    Effectue le clustering sur les séries temporelles en utilisant la norme L1
-    Entrées :
-        l_series : liste d'array numpy, chaque array correspond à une série temporelle
-    Sorties :
-        G : graphe networkx
-        m_similarite : matrice numpy (array de array de float)
-    """
-    # Calcul de la matrice de distance globale :
-    m_distance_globale = matrice_distance_globale_autres(l_series, distance_L1)
-
-    # Calcul de la matrice de similarité : 
-    m_similarite = matrice_similarite(m_distance_globale)
-
-    # # Transfo en graphe : 
-    G = transfo_graphe(m_similarite)
-
-    return G, m_similarite 
-
-def clustering_distance_L2(l_series) :
-    """
-    Effectue le clustering sur les séries temporelles en utilisant la norme L2
-    Entrées :
-        l_series : liste d'array numpy, chaque array correspond à une série temporelle
-    Sorties :
-        G : graphe networkx
-        m_similarite : matrice numpy (array de array de float)
-    """
-    # Calcul de la matrice de distance globale :
-    m_distance_globale = matrice_distance_globale_autres(l_series, distance_L2)
-
-    # Calcul de la matrice de similarité : 
-    m_similarite = matrice_similarite(m_distance_globale)
-
-    # # Transfo en graphe : 
-    G = transfo_graphe(m_similarite)
-
-    return G, m_similarite 
-
 def affiche_graphe(G):
     """
     Affiche le graphe pondéré G avec le poids des arrêtes arrondi au centième.
@@ -200,6 +80,137 @@ def lecture_initiale() :
     egalise_longueur_serie(dict_data)
 
     return dict_data["series"]
+
+def clustering_visibility_graph(l_series, nb_seg = 30) :
+    """
+    Effectue le clustering sur les séries temporelles en utilisant les graphes de visibilité pondérés
+    Entrées :
+        l_series : array d'array numpy, chaque array correspond à une série temporelle
+        nb_seg : entier, nombre de segments à découper pour chaque série
+    Sorties :
+        G : graphe networkx
+        m_similarite : matrice numpy (array de array de float)
+    """
+    # Découpage en un même nombre de segments :
+    segments_tot = []
+    for serie in l_series :
+        segments_tot.append(decoupe_segments(serie, nb_seg))
+
+    l_vecteurs = [[] for _ in range(nb_seg)]
+
+    # Transfo de chaque segment en weighted visibility graph :
+    for serie in segments_tot :
+        for idx_seg in range(nb_seg) :
+            seg = serie[idx_seg]
+            vg = NaturalVG(weighted="abs_slope")  # poids = pente absolue entre les points
+            vg.build(seg)
+            edges = vg.edges
+            n = len(seg)
+            # Matrice d'adjacence du graphe :
+            adj_matrix = np.zeros((n, n))
+            for u,v,w in edges:
+                adj_matrix[u, v] = w
+                adj_matrix[v, u] = w  # symétrique
+            # Calcul du vecteur :
+            vect = []
+            for u in range (len(adj_matrix)) :
+                composante = 0
+                for v in range(len(adj_matrix)) :
+                    if adj_matrix[u, v] != 0 :
+                        composante += 1
+                vect.append(composante)
+            l_vecteurs[idx_seg].append(vect)
+
+
+    # Calcul des matrices pour chaque segment :
+    matrices = []
+    for i in range(len(l_vecteurs)) :
+        matrices.append(matrice_segment(l_vecteurs[i]))
+
+
+    # Calcul de la matrice de distance globale :
+    m_distance_globale = matrice_distance_globale(matrices)
+    
+    # Calcul de la matrice de similarité : 
+    m_similarite = matrice_similarite(m_distance_globale)
+
+    # Passage par k-NN :
+    m_s_knn = knn_graph(m_similarite, 2)
+
+    # Transfo en graphe : 
+    G = transfo_graphe(m_s_knn)
+
+    return G, m_s_knn
+
+def clustering_distance_dtw(l_series) :
+    """
+    Effectue le clustering sur les séries temporelles en utilisant la distance DTW
+    Entrées :
+        l_series : liste d'array numpy, chaque array correspond à une série temporelle
+    Sorties :
+        G : graphe networkx
+        m_similarite : matrice numpy (array de array de float)
+    """
+    # Calcul de la matrice de distance globale :
+    m_distance_globale = matrice_distance_globale_autres(l_series, distance_dtw)
+
+    # Calcul de la matrice de similarité : 
+    m_similarite = matrice_similarite(m_distance_globale)
+
+    # Passage par k-NN :
+    m_s_knn = knn_graph(m_similarite, 2)
+
+    # Transfo en graphe : 
+    G = transfo_graphe(m_s_knn)
+
+    return G, m_s_knn
+
+def clustering_distance_L1(l_series, ) :
+    """
+    Effectue le clustering sur les séries temporelles en utilisant la norme L1
+    Entrées :
+        l_series : liste d'array numpy, chaque array correspond à une série temporelle
+    Sorties :
+        G : graphe networkx
+        m_similarite : matrice numpy (array de array de float)
+    """
+    # Calcul de la matrice de distance globale :
+    m_distance_globale = matrice_distance_globale_autres(l_series, distance_L1)
+
+    # Calcul de la matrice de similarité : 
+    m_similarite = matrice_similarite(m_distance_globale)
+
+    # Passage par k-NN :
+    m_s_knn = knn_graph(m_similarite, 2)
+
+    # Transfo en graphe : 
+    G = transfo_graphe(m_s_knn)
+
+    return G, m_s_knn
+
+def clustering_distance_L2(l_series) :
+    """
+    Effectue le clustering sur les séries temporelles en utilisant la norme L2
+    Entrées :
+        l_series : liste d'array numpy, chaque array correspond à une série temporelle
+    Sorties :
+        G : graphe networkx
+        m_similarite : matrice numpy (array de array de float)
+    """
+    # Calcul de la matrice de distance globale :
+    m_distance_globale = matrice_distance_globale_autres(l_series, distance_L2)
+
+    # Calcul de la matrice de similarité : 
+    m_similarite = matrice_similarite(m_distance_globale)
+
+    # Passage par k-NN :
+    m_s_knn = knn_graph(m_similarite, 2)
+
+    # Transfo en graphe : 
+    G = transfo_graphe(m_s_knn)
+
+    return G, m_s_knn
+
 
 if __name__ == "__main__" :
 
